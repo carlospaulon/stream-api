34a7aeb5204d95ab41b66e904a6c0c5fcf81e5b3
Carlos <paulonluis222@gmail.com>
2024-11-04 23:11:54 -0300

    atualizando desafio - stream

--- src/challenges_stream_api/Ch019.java
+++ src/challenges_stream_api/Ch019.java
@@ -10,13 +10,13 @@ public class Ch019 {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
         //elementos únicos
-        Set<Integer> numerosDiviseis = numeros.stream().filter(n -> n % 3 == 0 || n % 5 == 0).collect(Collectors.toSet());
+        Set<Integer> numerosDiviseis = numeros.stream().filter(n -> n % 3 == 0 && n % 5 == 0).collect(Collectors.toSet());
         int resultado = numerosDiviseis.stream().reduce(0, (n1, n2) -> n1 + n2);
         System.out.println("Set\nOs números da lista, divisíveis por três e por cinco são: " + numerosDiviseis);
         System.out.println("A soma dos números divisíveis três e cinco é: " + resultado);
 
         //elementos duplicados
-        List<Integer> numerosDiviseis1 = numeros.stream().filter(n -> n % 3 == 0 || n % 5 == 0).toList();
+        List<Integer> numerosDiviseis1 = numeros.stream().filter(n -> n % 3 == 0 && n % 5 == 0).toList();
         int resultado1 = numerosDiviseis1.stream().reduce(0, Integer::sum);
         System.out.println("\nList\nOs números da lista, divisíveis por três e por cinco são: " + numerosDiviseis1);
         System.out.println("A soma dos números divisíveis três e cinco é: " + resultado1);
